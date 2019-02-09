384b77f17844a89137d50ef47549e879f1f78947
3santiago3 <531151310@qq.com>
2019-02-09 09:56:56 +0800

    add eslint

--- js/index.js
+++ js/index.js
@@ -1,250 +1,250 @@
-let order = []; // the order of the lights on how they are going to flash
-let playerOrder = []; // the order that the player is pressing the lights in
-let flash; // the number of flashes, it means how many times should the lights flash
-let turn; // what turn we are on
-let good; // whether the player is doing well or not
-let compTurn; // whether it's the computer's turn or the player's turn
-let intervalId;
-let strict = false; // has the strict button been checked
-let noise = true;
-let on = false; // has the power button been checked (if the program has been turned on), if on equals true, the player can start pressing color buttons 
-let win;
+let order = [] // the order of the lights on how they are going to flash
+let playerOrder = [] // the order that the player is pressing the lights in
+let flash // the number of flashes, it means how many times should the lights flash
+let turn // what turn we are on
+let good // whether the player is doing well or not
+let compTurn // whether it's the computer's turn or the player's turn
+let intervalId
+let strict = false // has the strict button been checked
+let noise = true
+let on = false // has the power button been checked (if the program has been turned on), if on equals true, the player can start pressing color buttons 
+let win
 
-const turnCounter = document.querySelector("#turn");
-const topLeft = document.querySelector("#topleft");
-const topRight = document.querySelector("#topright");
-const bottomLeft = document.querySelector("#bottomleft");
-const bottomRight = document.querySelector("#bottomright");
-const strictButton = document.querySelector("#strict");
-const onButton = document.querySelector("#on");
-const startButton = document.querySelector("#start");
+const turnCounter = document.querySelector('#turn')
+const topLeft = document.querySelector('#topleft')
+const topRight = document.querySelector('#topright')
+const bottomLeft = document.querySelector('#bottomleft')
+const bottomRight = document.querySelector('#bottomright')
+const strictButton = document.querySelector('#strict')
+const onButton = document.querySelector('#on')
+const startButton = document.querySelector('#start')
 
 strictButton.addEventListener('change', () => {
-  strict = strictButton.checked;
-});
+	strict = strictButton.checked
+})
 
 onButton.addEventListener('change', () => {
-  on = onButton.checked;
+	on = onButton.checked
 
-  if (on) {
-    turnCounter.innerHTML = '-';
-  } else {
-    turnCounter.innerHTML = '';
-    clearColor();
-    clearInterval(intervalId);
-  }
-});
+	if (on) {
+		turnCounter.innerHTML = '-'
+	} else {
+		turnCounter.innerHTML = ''
+		clearColor()
+		clearInterval(intervalId)
+	}
+})
 
 startButton.addEventListener('click', () => {
-  if (on || win) {
-    play();
-  }
-});
+	if (on || win) {
+		play()
+	}
+})
 
 function play() {
-  win = false;
-  order = [];
-  playerOrder = [];
-  flash = 0;
-  intervalId = 0;
-  turn = 1;
-  turnCounter.innerHTML = 1;
-  good = true; // the player hasn't hit anything incorrect yet
+	win = false
+	order = []
+	playerOrder = []
+	flash = 0
+	intervalId = 0
+	turn = 1
+	turnCounter.innerHTML = 1
+	good = true // the player hasn't hit anything incorrect yet
 
-  for (let i = 0; i < 20; i++) {
-    order.push(Math.floor(Math.random() * 4) + 1);
-  }
+	for (let i = 0; i < 20; i++) {
+		order.push(Math.floor(Math.random() * 4) + 1)
+	}
 
-  compTurn = true;
+	compTurn = true
 
-  intervalId = setInterval(gameTurn, 800);
+	intervalId = setInterval(gameTurn, 800)
 }
 
 function gameTurn() {
-  // whenever on equals false the player cannot click any of these color buttons
-  on = false;
+	// whenever on equals false the player cannot click any of these color buttons
+	on = false
 
-  // if the number of times the lights have flashed equals the turn that we're on that means the computer's turn is over
-  if (flash == turn) {
-    clearInterval(intervalId);
-    compTurn = false;
-    clearColor();
-    on = true;
-  }
+	// if the number of times the lights have flashed equals the turn that we're on that means the computer's turn is over
+	if (flash == turn) {
+		clearInterval(intervalId)
+		compTurn = false
+		clearColor()
+		on = true
+	}
 
-  if (compTurn) {
-    clearColor();
+	if (compTurn) {
+		clearColor()
 
-    setTimeout(() => {
-      switch (order[flash]) {
-        case 1:
-          one();
-          break;
-        case 2:
-          two();
-          break;
-        case 3:
-          three();
-          break;
-        case 4:
-          four();
-          break;
-      }
+		setTimeout(() => {
+			switch (order[flash]) {
+			case 1:
+				one()
+				break
+			case 2:
+				two()
+				break
+			case 3:
+				three()
+				break
+			case 4:
+				four()
+				break
+			}
 
-      flash++;
-    }, 200);
-  }
+			flash++
+		}, 200)
+	}
 }
 
 function one() {
-  if (noise) {
-    let audio = document.getElementById("clip1");
-    audio.play();
-  }
-  noise = true;
-  topLeft.style.backgroundColor = "lightgreen";
+	if (noise) {
+		let audio = document.getElementById('clip1')
+		audio.play()
+	}
+	noise = true
+	topLeft.style.backgroundColor = 'lightgreen'
 }
 
 function two() {
-  if (noise) {
-    let audio = document.getElementById("clip2");
-    audio.play();
-  }
-  noise = true;
-  topRight.style.backgroundColor = "tomato";
+	if (noise) {
+		let audio = document.getElementById('clip2')
+		audio.play()
+	}
+	noise = true
+	topRight.style.backgroundColor = 'tomato'
 }
 
 function three() {
-  if (noise) {
-    let audio = document.getElementById("clip3");
-    audio.play();
-  }
-  noise = true;
-  bottomLeft.style.backgroundColor = "yellow";
+	if (noise) {
+		let audio = document.getElementById('clip3')
+		audio.play()
+	}
+	noise = true
+	bottomLeft.style.backgroundColor = 'yellow'
 }
 
 function four() {
-  if (noise) {
-    let audio = document.getElementById("clip4");
-    audio.play();
-  }
-  noise = true;
-  bottomRight.style.backgroundColor = "lightskyblue";
+	if (noise) {
+		let audio = document.getElementById('clip4')
+		audio.play()
+	}
+	noise = true
+	bottomRight.style.backgroundColor = 'lightskyblue'
 }
 
 function clearColor() {
-  topLeft.style.backgroundColor = "darkgreen";
-  topRight.style.backgroundColor = "darkred";
-  bottomLeft.style.backgroundColor = "goldenrod";
-  bottomRight.style.backgroundColor = "darkblue";
+	topLeft.style.backgroundColor = 'darkgreen'
+	topRight.style.backgroundColor = 'darkred'
+	bottomLeft.style.backgroundColor = 'goldenrod'
+	bottomRight.style.backgroundColor = 'darkblue'
 }
 
-topLeft.addEventListener('click', (event) => {
-  if (on) {
-    playerOrder.push(1);
-    check();
-    one();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 300);
-    }
-  }
+topLeft.addEventListener('click', () => {
+	if (on) {
+		playerOrder.push(1)
+		check()
+		one()
+		if(!win) {
+			setTimeout(() => {
+				clearColor()
+			}, 300)
+		}
+	}
 })
 
-topRight.addEventListener('click', (event) => {
-  if (on) {
-    playerOrder.push(2);
-    check();
-    two();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 300);
-    }
-  }
+topRight.addEventListener('click', () => {
+	if (on) {
+		playerOrder.push(2)
+		check()
+		two()
+		if(!win) {
+			setTimeout(() => {
+				clearColor()
+			}, 300)
+		}
+	}
 })
 
-bottomLeft.addEventListener('click', (event) => {
-  if (on) {
-    playerOrder.push(3);
-    check();
-    three();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 300);
-    }
-  }
+bottomLeft.addEventListener('click', () => {
+	if (on) {
+		playerOrder.push(3)
+		check()
+		three()
+		if(!win) {
+			setTimeout(() => {
+				clearColor()
+			}, 300)
+		}
+	}
 })
 
-bottomRight.addEventListener('click', (event) => {
-  if (on) {
-    playerOrder.push(4);
-    check();
-    four();
-    if(!win) {
-      setTimeout(() => {
-        clearColor();
-      }, 300);
-    }
-  }
+bottomRight.addEventListener('click', () => {
+	if (on) {
+		playerOrder.push(4)
+		check()
+		four()
+		if(!win) {
+			setTimeout(() => {
+				clearColor()
+			}, 300)
+		}
+	}
 })
 
 function check() {
-  // 你点击的那一项，就是 playerorder 的最后一项
-  // playerorder 的最后一项的数字不等于 order 对应的那一项的数字
-  // 将 good 设置为 false
-  if (playerOrder[playerOrder.length - 1] !== order[playerOrder.length - 1]) {
-    good = false;
-  }
+	// 你点击的那一项，就是 playerorder 的最后一项
+	// playerorder 的最后一项的数字不等于 order 对应的那一项的数字
+	// 将 good 设置为 false
+	if (playerOrder[playerOrder.length - 1] !== order[playerOrder.length - 1]) {
+		good = false
+	}
 
-  if (playerOrder.length == 3 && good) {
-    winGame();
-  }
+	if (playerOrder.length == 3 && good) {
+		winGame()
+	}
 
-  if (good == false) {
-    flashColor();
-    turnCounter.innerHTML = "NO!";
-    setTimeout(() => {
-      turnCounter.innerHTML = turn;
-      clearColor();
+	if (good == false) {
+		flashColor()
+		turnCounter.innerHTML = 'NO!'
+		setTimeout(() => {
+			turnCounter.innerHTML = turn
+			clearColor()
 
-      if (strict) {
-        play();
-      } else {
-        compTurn = true;
-        flash = 0;
-        playerOrder = [];
-        good = true;
-        intervalId = setInterval(gameTurn, 800);
-      }
-    }, 800);
+			if (strict) {
+				play()
+			} else {
+				compTurn = true
+				flash = 0
+				playerOrder = []
+				good = true
+				intervalId = setInterval(gameTurn, 800)
+			}
+		}, 800)
 
-    noise = false;
-  }
+		noise = false
+	}
 
-  if (turn == playerOrder.length && good && !win) {
-    turn++;
-    playerOrder = [];
-    compTurn = true;
-    flash = 0;
-    turnCounter.innerHTML = turn;
-    intervalId = setInterval(gameTurn, 800);
-  }
+	if (turn == playerOrder.length && good && !win) {
+		turn++
+		playerOrder = []
+		compTurn = true
+		flash = 0
+		turnCounter.innerHTML = turn
+		intervalId = setInterval(gameTurn, 800)
+	}
 
 }
 
 function winGame() {
-  flashColor();
-  turnCounter.innerHTML = "WIN!";
-  on = false;
-  win = true;
+	flashColor()
+	turnCounter.innerHTML = 'WIN!'
+	on = false
+	win = true
 }
 
 function flashColor() {
-  topLeft.style.backgroundColor = "lightgreen";
-  topRight.style.backgroundColor = "tomato";
-  bottomLeft.style.backgroundColor = "yellow";
-  bottomRight.style.backgroundColor = "lightskyblue";
+	topLeft.style.backgroundColor = 'lightgreen'
+	topRight.style.backgroundColor = 'tomato'
+	bottomLeft.style.backgroundColor = 'yellow'
+	bottomRight.style.backgroundColor = 'lightskyblue'
 }
